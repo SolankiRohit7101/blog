5f7a86163d6e4a60c6eee8cd5d17b17e5eaf7053
SolankiRohit7101 <SolankiRohit7101@gmail.com>
2024-06-26 14:51:48 +0530

    backend-v-1.11

--- Controller/userController.js
+++ Controller/userController.js
@@ -6,7 +6,7 @@ import jwtToken from "../utils/jwt.js";
 export const registerUser = async (req, res, next) => {
   let { name, password, email, profile_image } = req.body;
 
-  if (!profile_image || profile_image.length < 1) {
+  if (!profile_image) {
     profile_image =
       "https://res.cloudinary.com/dyxijouqy/image/upload/f_auto,q_auto/v1/blog_profile/dfanyr2nh3h4pwbkecmp";
   }
