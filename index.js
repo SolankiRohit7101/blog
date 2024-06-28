905a1076c7d5f8db86f717125325314982dbaf2b
SolankiRohit7101 <SolankiRohit7101@gmail.com>
2024-06-28 14:25:01 +0530

    backend-v-1.17

--- index.js
+++ index.js
@@ -20,10 +20,9 @@ app.use(
     credentials: true,
     allowedHeaders:
       "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-    optionsSuccessStatus: 200,
   })
 );
-app.use("*", (req, res, next) => res.send("<h1 >Page Not Found 404</h1>"));
+// app.use("*", (req, res, next) => res.send("<h1 >Page Not Found 404</h1>"));
 app.use("/api/user", userRouter);
 app.use("/api/blog", blogRouter);
 app.listen(process.env.PORT, () => {
